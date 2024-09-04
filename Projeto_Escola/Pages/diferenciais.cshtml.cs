2875131ef37e4dc87a36ae543d2440e54d0cbcdb
Carlos Eduardo <carlos_vk2012@hotmail.com>
2024-09-04 19:26:10 -0300

    Commit 2 dia 2

---
+++ Projeto_Escola/Pages/diferenciais.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Projeto_Escola.Pages
+{
+    public class diferenciaisModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Projeto_Escola/Pages/organograma.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Projeto_Escola.Pages
+{
+    public class organogramaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
