e43e5d84b1fe955f5edba5274e539d9860f5b4ee
Carlos Eduardo <carlos_vk2012@hotmail.com>
2024-09-06 23:12:42 -0300

    Commit sexta casa - Carlos

--- Projeto_Escola/obj/Debug/net6.0/Projeto_Escola.AssemblyInfo.cs
+++ Projeto_Escola/obj/Debug/net6.0/Projeto_Escola.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto_Escola")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35d615e203bce682a861828a94e7c7c04fd46b8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto_Escola")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto_Escola")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
